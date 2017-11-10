3eec77eda0f094af60b3dadd3333dd930c37bed8
Wenjing Wang <wenjing.liu.wang@gmail.com>
2017-11-10 12:08:38 -0500

    Add files via upload

--- Lab4/Swing/CheckBoxDemo.java
+++ Lab4/Swing/CheckBoxDemo.java
@@ -1,1 +1,23 @@
+package Swing;
 
+//Fig. 12.18: CheckBoxTest.java
+//Testing CheckBoxFrame.
+//Modified by S. Ranev
+//version: 1.17.2
+import javax.swing.JFrame;
+import java.awt.EventQueue;
+
+public class CheckBoxDemo {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				CheckBoxFrame checkBoxFrame = new CheckBoxFrame();
+				checkBoxFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				checkBoxFrame.setSize(400, 100); // set frame size
+				checkBoxFrame.setLocationByPlatform(true);
+				checkBoxFrame.setVisible(true); // display frame
+			}
+		});
+
+	} // end main
+} // end class CheckBoxDemo
